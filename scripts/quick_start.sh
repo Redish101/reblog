8456e624134d3a7595eff45f78ef1bcd7a21e8e8
Redish101 <i@redish101.top>
2024-04-21 17:06:52 +0800

    chore: `quick-start.sh`中的错误修复

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -6,6 +6,6 @@ cd ~/reblog
 
 curl https://github.com/redish101/reblog/raw/main/docker-compose.yml -o docker-compose.yml
 
-git clone https://github.com/redish101/reblog.git .
+git clone https://github.com/redish101/reblog.git
 
 docker-compose up -d
