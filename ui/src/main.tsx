2d12e40ceb8b38ed1b230ca93705f33983f8339c
Redish101 <i@redish101.top>
2024-04-24 21:34:44 +0800

    chore: 初始化前端工程及基本布局

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,7 +1,14 @@
+/**
+ * reblog dashboard
+ * @author Redish101 <i@redish101.top>
+ * @license GPL-3.0
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+import "@/styles/base.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dashboard',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
