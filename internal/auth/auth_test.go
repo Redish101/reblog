3fc3e20d1003bb6139912f8f9674d47e46b606d4
Redish101 <i@redish101.top>
2024-05-11 20:56:31 +0800

    fix: 修复测试用例的错误

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,13 +1,35 @@
 package auth
 
-import "testing"
+import (
+	"reblog/internal/model"
+	"reblog/internal/query"
+	"testing"
+)
 
 func TestSetKey(t *testing.T) {
 	SetKey()
 }
 
 func TestVerifyToken(t *testing.T) {
-	token := GetToken("testuser", "testpassword")
+	username := "testuser"
+	nickname := "testnickname"
+	email := "testemail"
+	password := "testpassword"
+
+	user := &model.User{
+		Username: username,
+		Nickname: nickname,
+		Email:    email,
+		Password: password,
+	}
+
+	err := query.User.Create(user)
+
+	if err != nil {
+		t.Errorf("创建用户失败: %v", err)
+	}
+
+	token := GetToken(username, password)
 	
 	tokenStatus := ValidToken(token)
 
