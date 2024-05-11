730e8aec61eb2e6dc2ff621dea2b8e671fed3d24
Redish101 <i@redish101.top>
2024-05-12 07:08:32 +0800

    Revert "fix: 修复测试用例的错误"
    This reverts commit 3fc3e20d1003bb6139912f8f9674d47e46b606d4.

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,35 +1,13 @@
 package auth
 
-import (
-	"reblog/internal/model"
-	"reblog/internal/query"
-	"testing"
-)
+import "testing"
 
 func TestSetKey(t *testing.T) {
 	SetKey()
 }
 
 func TestVerifyToken(t *testing.T) {
-	username := "testuser"
-	nickname := "testnickname"
-	email := "testemail"
-	password := "testpassword"
-
-	user := &model.User{
-		Username: username,
-		Nickname: nickname,
-		Email:    email,
-		Password: password,
-	}
-
-	err := query.User.Create(user)
-
-	if err != nil {
-		t.Errorf("创建用户失败: %v", err)
-	}
-
-	token := GetToken(username, password)
+	token := GetToken("testuser", "testpassword")
 	
 	tokenStatus := ValidToken(token)
 
