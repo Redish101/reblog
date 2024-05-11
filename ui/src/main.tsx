9364383f2af4f2cb4f47fddc942eddf7e00e8590
Redish101 <i@redish101.top>
2024-05-11 20:25:02 +0800

    chore: unocss配置

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,17 +1,17 @@
-/**
- * reblog dashboard
- * @author Redish101 <i@redish101.top>
- * @license GPL-3.0
- */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import '@/global.css'
 
-import "@/styles/base.css";
+import 'mdui'
+import { setColorScheme } from 'mdui'
+import 'mdui/mdui.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+setColorScheme('#2b6672')
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import UnoCSS from 'unocss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dashboard",
-  plugins: [react()],
+  base: '/dashboard',
+  plugins: [
+    react(),
+    UnoCSS()
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
