6f26f8f8dd1cf7d591c28859776e0ef0fe2b91d7
Redish101 <i@redish101.top>
2024-05-12 13:18:16 +0800

    feat: text button及outlined button

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,8 +2,15 @@ import Button from "./components/Button";
 
 export default function App() {
   return (
-    <div>
-      <Button variant="filled">Button</Button>
+    <div className="flex gap-1 p-24">
+      <div>
+        <h2 className="font-light text-3xl">Buttons</h2>
+        <div className="flex gap-1">
+          <Button variant="filled">Button</Button>
+          <Button variant="outlined">Button</Button>
+          <Button variant="text">Button</Button>
+        </div>
+      </div>
     </div>
   );
 }
--- ui/uno.config.ts
+++ ui/uno.config.ts
@@ -7,13 +7,18 @@ export default defineConfig({
     theme: {
         colors: {
             primary: '#2b6672',
-            secondly: '#3c727d'
+            secondary: '#3c727d',
+            bg: '#fbfcfd',
+            hover: '#eaf0f2'
         },
         height: {
             9: '2.5rem'
         },
         borderRadius: {
             DEFAULT: '2.5rem'
+        },
+        fontWeight: {
+            semibold: "500",
         },
         breakpoints: {
             5: '1.5rem'
