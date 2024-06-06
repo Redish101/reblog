ab91adf08cc65a97beee33487ce8e9af66f6c023
Redish101 <i@redish101.top>
2024-06-06 14:52:07 +0800

    chore: 完善布局
    Signed-off-by: Redish101 <i@redish101.top>

---
+++ ui/mock/version.ts
@@ -0,0 +1,10 @@
+export default {
+  "GET /api/version": {
+    success: true,
+    msg: "操作成功",
+    data: {
+      app_name: "reblog-dev.dev(mock)",
+      runtime: "go1.22",
+    },
+  },
+};
--- ui/src/layouts/index.tsx
+++ ui/src/layouts/index.tsx
@@ -1,10 +1,30 @@
-import { PageContainer, ProCard, ProLayout } from "@ant-design/pro-components";
+import { HomeFilled } from "@ant-design/icons";
+import {
+  DefaultFooter,
+  PageContainer,
+  ProCard,
+  ProLayout,
+} from "@ant-design/pro-components";
 import { Outlet, useLocation } from "umi";
 
 const Layout = () => {
   const location = useLocation();
   return (
-    <ProLayout title="reblog" siderWidth={216} location={location}>
+    <ProLayout
+      title="reblog"
+      siderWidth={216}
+      location={location}
+      route={{
+        path: "/",
+        routes: [
+          {
+            path: "/",
+            name: "首页",
+            icon: <HomeFilled />,
+          },
+        ],
+      }}
+    >
       <PageContainer>
         <ProCard style={{ minHeight: 700 }}>
           <Outlet />
