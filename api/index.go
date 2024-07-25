4078cf3dbfcb9947d53281775a6f32164d6cabf9
Redish101 <i@redish101.top>
2024-07-25 09:01:45 +0800

    feat: vercel支持

---
+++ api/index.go
@@ -0,0 +1,23 @@
+// reblog vercel 兼容层
+
+package handler
+
+import (
+	"net/http"
+	"reblog/internal/core"
+
+	"github.com/gofiber/fiber/v3/middleware/adaptor"
+)
+
+var srv http.Handler
+
+func init() {
+	app := core.NewApp()
+	app.Bootstrap()
+
+	srv = adaptor.FiberApp(app.Fiber())
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	srv.ServeHTTP(w, r)
+}
